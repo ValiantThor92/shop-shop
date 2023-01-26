e86d939f87089ba68dc809af26178127b0598268
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2023-01-25 17:07:47 -0800

    implement global state

---
+++ client/build/precache-manifest.9c9a85d2b5f7e16cee46424f7efcdceb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce1513b33ee7da3933db40b5c655aac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14233a0fa6f143d5ac5d",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "57b9f997f71f24112839",
+    "url": "/static/js/2.3d4b54e5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3d4b54e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14233a0fa6f143d5ac5d",
+    "url": "/static/js/main.1aa8af6d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.1aa8af6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
